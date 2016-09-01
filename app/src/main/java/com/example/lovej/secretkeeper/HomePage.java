f878e8e84cdbcd09ac4e400fe8075347cb7a899d
z674551360 <674551360@qq.com>
2016-09-01 18:01:40 +1200

    The "addSecret" function dont not working, it should add Textview to the scrollView

--- app/src/main/java/com/example/lovej/secretkeeper/HomePage.java
+++ app/src/main/java/com/example/lovej/secretkeeper/HomePage.java
@@ -2,17 +2,65 @@ package com.example.lovej.secretkeeper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * Created by lovej on 2016/8/21 0021.
+ * Created by FD-GHOST/lovej on 2016/9/01 1450.
  */
 public class HomePage extends AppCompatActivity {
-
+    private ScrollView secrets;
+    private TextView child;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_page);
+        init();
+    }
+
+    private void init() {
+        secrets = (ScrollView) findViewById(R.id.home_mid);
+        secrets.setOnTouchListener(new TouchListenerImpl());
+    }
+
+    private void addSecret() {
+        child = new TextView(null);
+        child.setText("Go Go Go");
+        secrets.addView(child);
+        //this function does not work
+    }
+
+    private class TouchListenerImpl implements OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int scrollY = view.getScrollY();
+                    int height = view.getHeight();
+                    int scrollViewMeasuredHeight = secrets.getChildAt(0).getMeasuredHeight();
+                    if (scrollY == 0) {
+                        Toast.makeText(HomePage.this, "Top", Toast.LENGTH_SHORT).show();
+                    }
+                    if ((scrollY + height) == scrollViewMeasuredHeight) {
+                        Toast.makeText(HomePage.this, "bot", Toast.LENGTH_SHORT).show();
+                        addSecret();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return false;
+        }
+
     }
 }
